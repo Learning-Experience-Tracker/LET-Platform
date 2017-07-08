c0080cdfdca441491127b5497d018297f522f0db
Mohammed Ghanem <mohammed_ghanem@outlook.com>
2017-07-08 15:36:54 +0300

    edit UserCourses model

--- modules/users/server/models/UserCourses.js
+++ modules/users/server/models/UserCourses.js
@@ -3,7 +3,9 @@ module.exports = function (sequelize, DataTypes) {
 	var UserCourses = sequelize.define('UserCourses',
 		{			
 			enroll_date : DataTypes.DATE,
-            enroll_times : DataTypes.INTEGER
+            enroll_times : DataTypes.INTEGER,
+			credits : {type:DataTypes.INTEGER , defaultValue:0},
+			final_result : DataTypes.ENUM('Distinction','Pass','Fail','Withdrawn')
 		}
 	);
 
