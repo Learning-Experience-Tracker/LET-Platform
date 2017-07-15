df47708e899de6ebbf257bd6850a07a915512d0f
Mohammed Ghanem <mohammed_ghanem@outlook.com>
2017-07-15 23:12:15 +0300

    complete activity create api + import Toast course data

---
+++ data/multiSourceDemo/genrateData.js
@@ -0,0 +1,336 @@
+var sequelize = require('../../config/sequelize'),
+    winston = require('../../config/winston'),
+    adminConfigs = require('../../config/env').adminConfigs,
+    fs = require('fs'),
+    async = require('async');
+
+sequelize.init(function (db) {
+
+    var admin = {};
+    var oranization = {};
+    var courseObject = {};
+    var usersMap = new Map();
+    var verbsMap = new Map();
+    var assessmentsMap = new Map();
+    var questionSMap = new Map();
+
+
+    async.series([
+        function (callback) {
+            addAdmin(callback)
+        },
+        function (callback) {
+            addOrganization(callback)
+        },
+        function (callback) {
+            addCourse(callback)
+        },
+        function (callback) {
+            addUsers(callback);
+        },
+        function (callback) {
+            addVerbs(callback)
+        },
+        function (callback) {
+            addResources(callback)
+        },function (callback){
+            addAsessments(callback);
+        }
+    ], function (err) {
+        winston.info('All objects are addeded');
+    });
+
+    function addAdmin(callback) {
+        db.User.find({
+                where: {
+                    username: 'admin'
+                }
+            })
+            .then(function (adminUser) {
+                if (!adminUser) { // no admin user registered
+                    winston.info('No admin user found, creating...');
+
+                    adminUser = db.User.build(adminConfigs);
+
+                    adminUser.save().then(function (newItem) {
+                        admin = newItem.dataValues;
+                        winston.info('Admin user created.');
+                        callback();
+                    });
+
+                } else {
+                    admin = adminUser.dataValues;
+                    winston.info('Admin user is already created.');
+                    callback();
+                }
+            })
+            .catch(function (error) {
+                winston.error('Error with admin user...');
+                winston.error(error);
+                callback(error);
+            });
+    }
+
+    function addOrganization(callback) {
+        db.Organization.find({
+                where: {
+                    name: 'Multi Source Org'
+                }
+            })
+            .then(function (existOranization) {
+                if (!existOranization) {
+                    winston.info('No Organization found, creating...');
+
+                    oranization = db.Organization.build({
+                        name: 'Multi Source Org',
+                        UserId: admin.id
+                    });
+
+                    oranization.save().then(function (newItem) {
+                        oranization = newItem.dataValues;
+                        winston.info('Organization created.');
+                        callback();
+                    });
+
+                } else {
+                    oranization = existOranization.dataValues;
+                    winston.info('Organization is already created.');
+                    callback();
+                }
+            })
+            .catch(function (error) {
+                winston.error('Error with Organization ...');
+                winston.error(error);
+                callback(error);
+            });
+    }
+
+    function addCourse(callback) {
+        db.Course.find({
+                where: {
+                    name: 'How to Make French Toast'
+                }
+            })
+            .then(function (existCourse) {
+                if (!existCourse) {
+                    winston.info('No Course found, creating...');
+
+                    course = db.Course.build({
+                        name: 'How to Make French Toast',
+                        startDate: new Date(),
+                        OrganizationId: oranization.id,
+                        id_IRI: "http://adlnet.gov/samples/course/api-jqm/"
+                    });
+
+                    course.save().then(function (newItem) {
+                        courseObject = newItem;
+                        winston.info('Course created.');
+                        callback();
+                    });
+
+                } else {
+                    course = existCourse.dataValues;
+                    winston.info('Course is already created.');
+                    callback();
+                }
+            })
+            .catch(function (error) {
+                winston.error('Error with Course ...');
+                winston.error(error);
+                callback(error);
+            });
+    }
+
+    function addUsers(callback) {
+
+        var users = [{
+            name: "Mohammed Ghanem",
+            username: "MG",
+            password: "MG",
+            email: "ghanem-mhd@adlnet.gov",
+            role: "student"
+        }, {
+            name: "Aalaa alhamwi",
+            username: "AA",
+            password: "AA",
+            email: "aalaa.alhamwi@adlnet.gov",
+            role: "student"
+        }, {
+            name: "Bassel shemali",
+            username: "BS",
+            password: "BS",
+            email: "bass3l@adlnet.gov",
+            role: "student"
+        }, {
+            name: "Hania Almalki",
+            username: "HA",
+            password: "HA",
+            email: "haniah-mk@adlnet.gov",
+            role: "student"
+        }];
+
+        async.each(users, function (user, callback) {
+            var userObject = db.User.build(user);
+            userObject.save().then(function () {
+                winston.info('User Created');
+                userObject.addCourse(courseObject, {
+                    enroll_date: Date.now(),
+                    enroll_times: 1
+                }).then(function () {
+                    callback();
+
+                });
+            });
+        }, function () {
+            winston.info('All users added...');
+            callback(null);
+        });
+    }
+
+    function addVerbs(callback) {
+
+        var verbs = [{
+            name: 'answered',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/answered'
+        }, {
+            name: 'attempted',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/attempted'
+        }, {
+            name: 'completed',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/completed'
+        }, {
+            name: 'clicked',
+            id_IRI: 'https://w3id.org/xapi/let/verbs/clicked'
+        }, {
+            name: 'loggedin',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/logged-in'
+        }, {
+            name: 'commented',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/commented'
+        }, {
+            name: 'launched',
+            id_IRI: 'https://w3id.org/xapi/adl/verbs/launched'
+        }];
+
+        winston.info('Start verbs import series...');
+
+        async.eachSeries(verbs, function (verbObject, callback) {
+            db.Verb.find({
+                    where: {
+                        id_IRI: verbObject.id_IRI
+                    }
+                })
+                .then(function (verb) {
+                    if (!verb) {
+                        winston.info('No verb found with ' + verbObject.id_IRI);
+                        verb = db.Verb.build(verbObject);
+                        verb.save().then(function (newItem) {
+                            winston.info('Verb Created');
+                            verbsMap[newItem.dataValues.id_IRI] = newItem.dataValues;
+                            callback(); // process next statement    
+                        });
+
+                    } else {
+                        winston.info('Verb is already created ' + verbObject.id_IRI);
+                        verbsMap[verb.dataValues.id_IRI] = verb.dataValues;
+                        callback(); // process next statement
+                    }
+                })
+                .catch(function (error) {
+                    winston.error('Error adding verb');
+                    winston.error(error);
+                    callback(error);
+                });
+        }, function () {
+            winston.info('All verbs added...');
+            callback(null);
+        });
+    }
+
+    function addResources(callback) {
+        var resources = [{
+                id_IRI: courseObject.dataValues.id_IRI + "toc",
+                type: "homepage",
+                name: "How to Make French Toast Homepage",
+                CourseId: course.id
+            }, {
+                id_IRI: courseObject.dataValues.id_IRI + "01-intro",
+                type: "content",
+                name: " Introduction 01",
+                CourseId: course.id
+            }, {
+                id_IRI: courseObject.dataValues.id_IRI + "02-ingredients",
+                type: "content",
+                name: "Ingredients 02",
+                CourseId: course.id
+            }, {
+                id_IRI: courseObject.dataValues.id_IRI + "03-steps",
+                type: "content",
+                name: "Steps 03",
+                CourseId: course.id
+            },
+            {
+                id_IRI: +courseObject.dataValues.id_IRI + "04-video",
+                type: "content",
+                name: "Video 04",
+                CourseId: course.id
+            }, {
+                id_IRI: courseObject.dataValues.id_IRI + "glossary",
+                type: "glossary",
+                name: "Course Glossary",
+                CourseId: course.id
+            },
+            {
+                id_IRI: courseObject.dataValues.id_IRI + "help",
+                type: "wiki",
+                name: "Wiki Help",
+                CourseId: course.id
+            }
+        ];
+
+
+        db.Resource.bulkCreate(resources).then(function () {
+            winston.info("resources added");
+            callback();
+        }).catch(function (error) {
+            winston.error(error);
+        });
+    }
+
+    function addAsessments(callback) {
+        var assessment = {
+            name: "Quiz",
+            id_IRI: "http://adlnet.gov/samples/course/api-jqm/05-quiz"
+        };
+
+
+        assessment = db.Assessment.build(assessment);
+
+        assessment.save().then(function (newItem) {
+            winston.info('Assessment created.');
+
+            var questions = [{
+                    name: "Question 1",
+                    id_IRI: "http://adlnet.gov/samples/course/api-jqm/#q1",
+                    AssessmentId: newItem.id
+                },
+                {
+                    name: "Question 2",
+                    id_IRI: "http://adlnet.gov/samples/course/api-jqm/#q2",
+                    AssessmentId: newItem.id
+
+                },
+                {
+                    name: "Question 3",
+                    id_IRI: "http://adlnet.gov/samples/course/api-jqm/#q3",
+                    AssessmentId: newItem.id
+                }
+            ];
+
+            db.Question.bulkCreate(questions).then(function () {
+                winston.info("questions added");
+                callback();
+            });
+        });
+    }
+});
--- modules/activities/server/controllers/ActivitiesController.js
+++ modules/activities/server/controllers/ActivitiesController.js
@@ -1,79 +1,249 @@
 'use strict';
 
 var db = require('./../../../../config/sequelize'),
-    winston = require('./../../../../config/winston');
+    winston = require('./../../../../config/winston'),
+    async = require('async');
 
-module.exports.get = function(req, res){
+module.exports.get = function (req, res) {
     res.end();
 }
 
-module.exports.getAll = function(req, res){
+module.exports.getAll = function (req, res) {
     db.Statement
-      .findAll({
-          where :{},
-          order : [
-            ['id', 'ASC']
-          ],
-          include : [
-              {model : db.Verb},
-              {model : db.User},
-              {model : db.Assessment},
-              {model : db.Resource},
-              {model : db.Question}
-        ]
-      }).then(function(statements){
-        return res.json(statements);
-    }).catch(function(err){
-        winston.error(err);
-        res.status(500).end();
-    });
-}
-
-module.exports.page = function(req, res){
-    db.Statement
-      .findAll({
-          where :{},
-          order : [
+        .findAll({
+            where: {},
+            order: [
                 ['id', 'ASC']
-          ],
-          include : [
-              {model : db.Verb},
-              {model : db.User},
-              {model : db.Assessment},
-              {model : db.Resource},
-              {model : db.Question}
-        ],
-        limit: req.body.limit,
-        offset: req.body.offset,
-      }).then(function(statements){
-        return res.json(statements);
-    }).catch(function(err){
-        winston.error(err);
-        res.status(500).end();
-    });
+            ],
+            include: [{
+                    model: db.Verb
+                },
+                {
+                    model: db.User
+                },
+                {
+                    model: db.Assessment
+                },
+                {
+                    model: db.Resource
+                },
+                {
+                    model: db.Question
+                }
+            ]
+        }).then(function (statements) {
+            return res.json(statements);
+        }).catch(function (err) {
+            winston.error(err);
+            res.status(500).end();
+        });
 }
 
-module.exports.getCount = function(req, res){
+module.exports.page = function (req, res) {
     db.Statement
-      .findAll().then(function(statements){
-        return res.json(statements.length);
-    }).catch(function(err){
-        winston.error(err);
-        res.status(500).end();
-    });
+        .findAll({
+            where: {},
+            order: [
+                ['id', 'ASC']
+            ],
+            include: [{
+                    model: db.Verb
+                },
+                {
+                    model: db.User
+                },
+                {
+                    model: db.Assessment
+                },
+                {
+                    model: db.Resource
+                },
+                {
+                    model: db.Question
+                }
+            ],
+            limit: req.body.limit,
+            offset: req.body.offset,
+        }).then(function (statements) {
+            return res.json(statements);
+        }).catch(function (err) {
+            winston.error(err);
+            res.status(500).end();
+        });
 }
 
-module.exports.getVerbs = function(req, res){
-    db.Verb.findAll().then(function(verbs){
+module.exports.getCount = function (req, res) {
+    db.Statement
+        .findAll().then(function (statements) {
+            return res.json(statements.length);
+        }).catch(function (err) {
+            winston.error(err);
+            res.status(500).end();
+        });
+}
+
+module.exports.getVerbs = function (req, res) {
+    db.Verb.findAll().then(function (verbs) {
         res.json(verbs);
-    }).catch(function(error){
+    }).catch(function (error) {
         winston.error(error);
         res.status(500).end();
     });
 }
 
-module.exports.create = function (req,res){
-    console.log(JSON.stringify(req.body, null, 2));
-    console.log("-----------------------------");
-    res.end();
+module.exports.create = function (req, res) {
+    var rawStatement = req.body;
+
+
+    var verbObject = rawStatement.verb;
+    var actorObject = rawStatement.actor;
+    var parentObject = rawStatement.context.contextActivities.parent[0];
+    var objectObject = rawStatement.object;
+
+
+    //winston.info(JSON.stringify(verbObject,null,2));
+    //winston.info(JSON.stringify(objectObject,null,2));
+    async.parallel({
+        course: function (callback) {
+            db.Course.findOne({
+                where: {
+                    id_IRI: parentObject.id
+                }
+            }).then(function (course) {
+                if (!course) {
+                    callback("Course Not Found " + parentObject.id, null);
+                } else {
+                    callback(null, course);
+                }
+            });
+        },
+        user: function (callback) {
+            db.User.findOne({
+                where: {
+                    email: actorObject.mbox
+                }
+            }).then(function (user) {
+                if (!user) {
+                    callback("Actor Not Found " + actorObject.id, null);
+                } else {
+                    callback(null, user);
+                }
+            });
+        },
+        object: function (callback) {
+            async.tryEach([ // try each task in series but stops whenever any of the functions were successful
+                function getQuestion(callback) {
+                    db.Question.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (question) {
+                        if (!question) {
+                            callback("Question Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: question,
+                                type: 'question'
+                            });
+                        }
+                    });
+                },
+                function getAssessment(callback) {
+                    db.Assessment.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (assessment) {
+                        if (!assessment) {
+                            // fail try question
+                            callback("Assessment Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: assessment,
+                                type: 'assessment'
+                            });
+                        }
+                    });
+                },
+                function getResource(callback) {
+                    db.Resource.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (resource) {
+                        if (!resource) {
+                            // fail try assessment
+                            callback("Resource Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: resource,
+                                type: 'resource'
+                            });
+                        }
+                    });
+                },
+            ], function (err, results) {
+                if (err) {
+                    winston.error("Object Not Found " + objectObject.id);
+                    callback("Object Not Found", null);
+                } else {
+                    callback(null, results);
+                }
+            });
+        },
+        verb: function (callback) {
+            db.Verb.findOne({
+                where: {
+                    id_IRI: verbObject.id
+                }
+            }).then(function (verb) {
+                if (!verb) {
+                    winston.error("Verb Not Found " + verbObject.id);
+                    callback("Verb Not Found", null);
+                } else {
+                    callback(null, verb);
+                }
+            });
+        }
+    }, function (err, results) {
+        if (err) {
+            res.status(500).end();
+            return;
+        } else {
+            winston.info("Course : " + results.course.name);
+            winston.info("Actor : " + results.user.name);
+            winston.info("Object : " + results.object.type);
+            winston.info("Verb : " + results.verb.name);
+
+    
+            var statementObject = {
+                UserId: results.user.id,
+                VerbId: results.verb.id,
+                timestamp: new Date(),
+                stored: new Date(),
+                platform: "Website",
+                xapi_statement : rawStatement
+            };
+            console.log(results.object.object.id);
+
+            if (results.object.type == "resource") {
+                statementObject.ResourceId = results.object.object.id;
+            }
+
+            if (results.object.type == "assessment"){
+                statementObject.AssessmentId = results.object.object.id;
+            }
+
+            if (results.object.type == "question"){
+                statementObject.QuestionId = results.object.object.id;
+            }
+
+            var statement = db.Statement.build(statementObject);
+            statement.save().then(function (newItem) {
+                winston.info('Added new activity record');
+                res.json("Ok");
+            });
+        }
+        winston.info("---------------------------------");
+    });
 }
