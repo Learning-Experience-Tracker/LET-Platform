f7484ae99f7281dfe55e8da232c9b78a0b6e7092
Mohammed Ghanem <mohammed_ghanem@outlook.com>
2017-07-19 07:10:32 +0300

    edit api doc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "Welcome to Learning Experience Tracking API Documentation",
   "title": "LET API Documentation",
-  "url": "https://localhost:3000",
+  "url": "",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-16T20:56:11.259Z",
+    "time": "2017-07-17T17:22:34.664Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
