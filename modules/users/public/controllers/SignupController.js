4afedbc84566f7783008bd6ff6ce3aa3a01e8564
Mohammed Ghanem <mohammed_ghanem@outlook.com>
2017-07-19 15:42:15 +0300

    edit signUp

--- modules/users/public/controllers/SignupController.js
+++ modules/users/public/controllers/SignupController.js
@@ -28,7 +28,7 @@
                return;
            }
 
-           UserService.signup({name : vm.name, username : vm.username,email: vm.email, password : vm.password, role : vm.role });
+           UserService.signup({name : vm.name, username : vm.username,email: vm.email, password : vm.password, role : 'admin' });
        }
 
        $rootScope.$on('signupFailed', function(){
