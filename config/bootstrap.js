9d0267c5841a165cd3a7c9f8f02ed2e7346b7e8c
Mohammed Ghanem <mohammed_ghanem@outlook.com>
2017-07-22 08:34:23 +0300

    bootstrap verbs

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,29 +1,85 @@
-
 var db = require('./sequelize'),
     adminConfigs = require('./env').adminConfigs,
-    winston = require('./winston');
+    winston = require('./winston'),
+    async = require('async');
 
-module.exports = function(){
+module.exports = function () {
 
     //bootstraping with admin user
-    db.User.find({ where : { username : 'admin' } })
-      .then(function(adminUser){
-        if(!adminUser){ // no admin user registered
-            winston.info('No admin user found, creating...');
-            
-            adminUser = db.User.build(adminConfigs);
+    db.User.find({
+            where: {
+                username: 'admin'
+            }
+        })
+        .then(function (adminUser) {
+            if (!adminUser) { // no admin user registered
+                winston.info('No admin user found, creating...');
 
-            adminUser.save().then(function(){
-                winston.info('Admin user created.');                
+                adminUser = db.User.build(adminConfigs);
+
+                adminUser.save().then(function () {
+                    winston.info('Admin user created.');
+                });
+
+            } else
+                winston.info('Admin user is already created.');
+        })
+        .catch(function (error) {
+            winston.error('Error bootstraping with admin user...');
+            winston.error(error);
+        });
+
+
+    var verbs = [{
+        name: 'answered',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/answered'
+    }, {
+        name: 'attempted',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/attempted'
+    }, {
+        name: 'completed',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/completed'
+    }, {
+        name: 'clicked',
+        id_IRI: 'https://w3id.org/xapi/let/verbs/clicked'
+    }, {
+        name: 'loggedin',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/logged-in'
+    }, {
+        name: 'commented',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/commented'
+    }, {
+        name: 'launched',
+        id_IRI: 'https://w3id.org/xapi/adl/verbs/launched'
+    }];
+
+    winston.info('Start verbs import series...');
+
+    async.eachSeries(verbs, function (verbObject, callback) {
+        db.Verb.find({
+                where: {
+                    id_IRI: verbObject.id_IRI
+                }
+            })
+            .then(function (verb) {
+                if (!verb) {
+                    winston.info('No verb found with ' + verbObject.id_IRI);
+                    verb = db.Verb.build(verbObject);
+                    verb.save().then(function (newItem) {
+                        winston.info('Verb Created');
+                        callback(); // process next statement    
+                    });
+
+                } else {
+                    winston.info('Verb is already created ' + verbObject.id_IRI);
+                    callback(); // process next statement
+                }
+            })
+            .catch(function (error) {
+                winston.error('Error adding verb');
+                winston.error(error);
             });
-
-        }else
-            winston.info('Admin user is already created.');
-      })
-      .catch(function(error){
-          winston.error('Error bootstraping with admin user...');
-          winston.error(error);
-      });
-
-
+    }, function () {
+        winston.info('All verbs added...');
+    });
 }
--- modules/activities/server/controllers/XapiController.js
+++ modules/activities/server/controllers/XapiController.js
@@ -4,7 +4,8 @@ var db = require('./../../../../config/sequelize'),
     env = require('./../../../../config/env'),
     winston = require('./../../../../config/winston'),
     async = require('async'),
-    request = require('request');
+    request = require('request'),
+    moment = require('moment');
 
 var options = {
     method: 'post',
@@ -19,29 +20,189 @@ var options = {
 
 module.exports.statements = function (req, res) {
     var rawStatement;
-    if (req.body instanceof Array){
-        rawStatement = req.body[0]; 
-    }else{
+    if (req.body instanceof Array) {
+        rawStatement = req.body[0];
+    } else {
         rawStatement = req.body;
     }
     var verbObject = rawStatement.verb;
     var actorObject = rawStatement.actor;
     var objectObject = rawStatement.object;
+    var parentObject = rawStatement.context.contextActivities;
+    var platform = rawStatement.context.platform;
 
-    objectObject.extensions = null;
+    var platform = rawStatement.context.platform;
+    var timestamp = moment(rawStatement.timestamp).toDate();
+    var stored = moment(rawStatement.stored).toDate();
 
 
-    console.log(JSON.stringify(verbObject, null, 2));
+    var courseID;
+    if (parentObject.hasOwnProperty('grouping')) {
+        courseID = parentObject.grouping[1].id;
+    } else {
+        courseID = parentObject.parent[0].id;
+    }
+
+
+    /*console.log(verbObject.id);
     console.log("--------------------------------------------------------");
-    console.log(JSON.stringify(actorObject, null, 2));
+    console.log(actorObject.mbox);
     console.log("--------------------------------------------------------");
-    console.log(JSON.stringify(objectObject, null, 2));
+    console.log(objectObject.id);
     console.log("--------------------------------------------------------");
+    console.log(courseID);
+    console.log("--------------------------------------------------------");
+    console.log(platform);
+    console.log("--------------------------------------------------------");
+    console.log(timestamp);
+    console.log("--------------------------------------------------------");*/
+
+    async.parallel({
+        course: function (callback) {
+            db.Course.findOne({
+                where: {
+                    id_IRI: courseID
+                }
+            }).then(function (course) {
+                if (!course) {
+                    callback("Course Not Found " + courseID, null);
+                } else {
+                    callback(null, course);
+                }
+            });
+        },
+        user: function (callback) {
+            db.User.findOne({
+                where: {
+                    email: actorObject.mbox
+                }
+            }).then(function (user) {
+                if (!user) {
+                    callback("Actor Not Found " + actorObject.mbox, null);
+                } else {
+                    callback(null, user);
+                }
+            });
+        },
+        object: function (callback) {
+            async.tryEach([ // try each task in series but stops whenever any of the functions were successful
+                function getQuestion(callback) {
+                    db.Question.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (question) {
+                        if (!question) {
+                            callback("Question Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: question,
+                                type: 'question'
+                            });
+                        }
+                    });
+                },
+                function getAssessment(callback) {
+                    db.Assessment.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (assessment) {
+                        if (!assessment) {
+                            // fail try question
+                            callback("Assessment Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: assessment,
+                                type: 'assessment'
+                            });
+                        }
+                    });
+                },
+                function getResource(callback) {
+                    db.Resource.findOne({
+                        where: {
+                            id_IRI: objectObject.id
+                        }
+                    }).then(function (resource) {
+                        if (!resource) {
+                            // fail try assessment
+                            callback("Resource Not Found " + objectObject.id, null);
+                        } else {
+                            callback(null, {
+                                object: resource,
+                                type: 'resource'
+                            });
+                        }
+                    });
+                },
+            ], function (err, results) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, results);
+                }
+            });
+        },
+        verb: function (callback) {
+            db.Verb.findOne({
+                where: {
+                    id_IRI: verbObject.id
+                }
+            }).then(function (verb) {
+                if (!verb) {
+                    callback("Verb Not Found " + verbObject.id, null);
+                } else {
+                    callback(null, verb);
+                }
+            });
+        }
+    }, function (err, results) {
+        if (err) {
+            winston.error(err);
+            res.status(500).end();
+            return;
+        } else {
+            winston.info("Course : " + results.course.name);
+            winston.info("Actor : " + results.user.name);
+            winston.info("Object : " + results.object.type);
+            winston.info("Verb : " + results.verb.name);
+
+
+            var statementObject = {
+                UserId: results.user.id,
+                VerbId: results.verb.id,
+                timestamp: timestamp,
+                stored: stored,
+                platform: platform,
+                xapi_statement: rawStatement,
+                CourseId : results.course.id
+            };
+            console.log(results.object.object.id);
+
+            if (results.object.type == "resource") {
+                statementObject.ResourceId = results.object.object.id;
+            }
+
+            if (results.object.type == "assessment") {
+                statementObject.AssessmentId = results.object.object.id;
+            }
+
+            if (results.object.type == "question") {
+                statementObject.QuestionId = results.object.object.id;
+            }
+
+            var statement = db.Statement.build(statementObject);
+            statement.save().then(function (newItem) {
+                winston.info('Added new activity record');
+                res.json("Ok");
+            });
+        }
+        winston.info("---------------------------------");
+    });
 
     /*options.body = req.body;
     request(options, (error, response, body) => {
         console.log(body)
     });*/
-
-    res.end();
 }
